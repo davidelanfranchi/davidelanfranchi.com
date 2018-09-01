65375461b67aa6c2f8c4102c4959dfcff6ab70b4
davidelanfranchi <davide.lanfra@gmail.com>
2018-09-01 04:30:56 +0200

    Update 2018-09-01T02:30:54.553Z

--- readme.md
+++
@@ -1,2 +0,0 @@
-
-# Personal web page
--- src/assets/js/components/helpers.js
+++
@@ -1,8 +0,0 @@
-export const isTouch = (() =>
-  "ontouchstart" in window ||
-  navigator.MaxTouchPoints > 0 ||
-  navigator.msMaxTouchPoints > 0)();
-
-export const isSafari = (() =>
-  navigator.userAgent.indexOf("Safari") != -1 &&
-  navigator.userAgent.indexOf("Chrome") == -1)();
